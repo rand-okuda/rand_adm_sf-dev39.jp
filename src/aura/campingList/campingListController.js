c4571719c92979509cb96089df5a77a3dccb69fb
okuda <okuda@RD12PC071N>
2018-02-09 18:43:28 +0900

    6. フォームを使用したデータの入力 Challenge完了

---
+++ src/aura/campingList/campingListController.js
@@ -0,0 +1,23 @@
+({
+
+    clickCreateItem : function(component, event) {
+
+        var validItem = component.find('newitemeform').reduce(function (validSoFar, inputCmp) {
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+
+        if(validItem){
+            var newItem = component.get("v.newItem");
+            var theItems = component.get("v.items"); 
+
+            theItems.push(newItem);
+
+            component.set("v.items", theItems);
+            component.set("v.newItem", { 'sobjectType': 'Camping_Item__c', 'Name': '', 'Quantity__c': 0, 'Price__c': 0, 'Packed__c': false })
+
+        }
+        	
+    }
+
+})
