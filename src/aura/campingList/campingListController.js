4b421d5e8627de7685c788f6c9dfa13c25b95a79
okuda <okuda@RD12PC061N>
2018-02-16 10:38:38 +0900

    campingListHelperからメソッドをcampingListControllerに移動しChallengeクリア。

--- src/aura/campingList/campingListController.js
+++ src/aura/campingList/campingListController.js
@@ -2,14 +2,58 @@
 
     handleAddItem: function(component, event, helper) {
         var newItem = event.getParam("item");
-        helper.createItem(component, newItem);
+        createItem(component, newItem);
     }
 
     ,
 
     handleChangeItem: function(component, event, helper) {
         var updatedItem = event.getParam("item");
-        helper.updateItem(component, updatedItem);
+        updateItem(component, updatedItem);
+    }
+
+    ,
+
+    createItem: function(component, item) {
+        
+        this.saveItem(component, item, function(response){
+            
+            var state = response.getState();
+            
+            if (state === "SUCCESS") {
+                var items = component.get("v.items");
+                items.push(response.getReturnValue());
+                component.set("v.items", items);
+            }
+            
+        });
+        
+    }
+    
+    ,
+    
+    updateItem: function(component, item) {
+        
+        this.saveItem(component, item);
+        
+    }
+    
+    ,
+    
+    saveItem: function(component, item, callback) {
+        
+        var action = component.get("c.saveItem");
+        
+        action.setParams(
+            { "item": item }
+        );
+        
+        if (callback) {
+            action.setCallback(this, callback);
+        }
+        
+        $A.enqueueAction(action);
+        
     }
 
     ,
