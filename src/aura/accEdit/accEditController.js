4cf5bfbeedf837f73f9796b4f566833378a8799d
okuda <okuda@RD12PC071N>
2018-02-16 19:58:08 +0900

    レコードの変更およびエラーの処理Challenge成功。コントローラー不完全。

--- src/aura/accEdit/accEditController.js
+++ src/aura/accEdit/accEditController.js
@@ -2,7 +2,8 @@
 
     handleSaveRecord: function(component, event, helper) {
 
-        component.find("recordEditor").saveRecord($A.getCallback(function(saveResult) {
+        var recordSaver = component.find("recordEditor");
+        recordSaver.saveRecord($A.getCallback(function(saveResult) {
 
             if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
                 console.log("Save completed successfully.");
@@ -11,10 +12,17 @@
                 console.log("User is offline, device doesn't support drafts.");
 
             } else if (saveResult.state === "ERROR") {
+                var errMsg = "";
+                for (var i = 0; i < saveResult.error.length; i++) {
+                    errMsg += saveResult.error[i].message + "\n";
+                }
+                component.set("v.recordSaveError", errMsg);
                 console.log('Problem saving record, error: ' +  JSON.stringify(saveResult.error));
 
             } else {
+                component.set("v.recordSaveError", "");
                 console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+
             }
 
         }));
